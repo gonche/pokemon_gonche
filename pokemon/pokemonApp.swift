0e07208d76021f90c83d4e4d60f416e53a726e85
Andy Gardner <agardner@consequentialrobotics.com>
2022-08-03 15:18:09 +0100

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Andrew Gardner on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
