177715eba0f580bcd090492c6a917d82a6408e7d
Andy Gardner <agardner@consequentialrobotics.com>
2022-08-06 22:02:56 +0100

    add progress spinner

--- pokemon/Spinner.swift
+++ pokemon/Spinner.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+struct Spinner: UIViewRepresentable {
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(style: style)
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        return spinner
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
