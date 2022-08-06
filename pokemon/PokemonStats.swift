762e369d4b531987e70064b80a3b7f1343c97180
Andy Gardner <agardner@consequentialrobotics.com>
2022-08-06 22:02:27 +0100

    add detail view for displaying pokemon stats

--- pokemon/PokemonStats.swift
+++ pokemon/PokemonStats.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct PokemonStats : View {
+    let name: String
+
+    var body: some View {
+        VStack {
+            Text(name)
+        }
+        .navigationTitle("")
+    }
+}
