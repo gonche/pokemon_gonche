353c2f838e9fd703270b24685b902c0aa3504f19
Andy Gardner <agardner@consequentialrobotics.com>
2022-08-06 22:01:44 +0100

    add caching of url link

--- pokemon/PokemonImage.swift
+++ pokemon/PokemonImage.swift
@@ -5,4 +5,33 @@
 //  Created by Andrew Gardner on 05/08/2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct PokemonImage: View {
+    var imageLink = ""
+    @State private var pokemonSprite = ""
+
+    var body: some View {
+        AsyncImage(url: URL(string: pokemonSprite))
+            .frame(width: 75, height: 75)
+            .onAppear {
+                let loadedData = UserDefaults.standard.string(forKey: imageLink)
+                if loadedData == nil {
+                    getSprite(url: imageLink)
+                    UserDefaults.standard.set(imageLink, forKey: imageLink)
+                }else{
+                    getSprite(url: loadedData!)
+                }
+            }
+            .clipShape(Circle())
+            .foregroundColor(Color.gray.opacity(0.60))
+    }
+    func getSprite(url: String){
+        var tempSprite: String?
+
+        PokemonSelectedApi().getResults(url: url) { sprite in
+            tempSprite = sprite.front_default
+            self.pokemonSprite = tempSprite ?? "placeholder"
+        }
+    }
+}
